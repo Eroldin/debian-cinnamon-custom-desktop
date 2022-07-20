772bdb0b7e34790721b8bb7d1fd5dd8069d564a1
Eroldin <54679426+Eroldin@users.noreply.github.com>
2022-07-20 12:27:35 +0200

    Added Vivaldi ARM64 to the script

--- cinnamon-desktop.sh
+++ cinnamon-desktop.sh
@@ -245,13 +245,26 @@ if [ "$FLATPAK" != flatpak ]; then
 		fi
 	else
 		if [ $(echo "$BROWSER" | grep -o vivaldi-stable || true) ]; then
-			if [ ! -f /etc/apt/sources.list.d/vivaldi.list ]; then 
-				aria2c -d /tmp https://repo.vivaldi.com/archive/linux_signing_key.pub
-				sudo apt-key add /tmp/linux_signing_key.pub
-				echo "deb https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
-				sudo apt update
+			if [ "$CPUAAMD" = AuthenticAMD ]; then
+				if [ ! -f /etc/apt/sources.list.d/vivaldi.list ]; then 
+					aria2c -d /tmp https://repo.vivaldi.com/archive/linux_signing_key.pub
+					sudo apt-key add /tmp/linux_signing_key.pub
+					echo "deb https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
+					sudo apt update
+				fi
+				sudo apt install -y $BROWSER "$NIR"
+			elif [ "$CPUAAMD" = GenuineIntel ]; then
+				if [ ! -f /etc/apt/sources.list.d/vivaldi.list ]; then 
+					aria2c -d /tmp https://repo.vivaldi.com/archive/linux_signing_key.pub
+					sudo apt-key add /tmp/linux_signing_key.pub
+					echo "deb https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
+					sudo apt update
+				fi
+				sudo apt install -y $BROWSER "$NIR"
+			else
+				aria2c -d /tmp https://downloads.vivaldi.com/snapshot/install-vivaldi.sh
+				sudo sh /tmp/install-vivaldi --final
 			fi
-			sudo apt install -y $BROWSER "$NIR"
 		else
 			sudo apt install -y  $BROWSER "$NIR"
 			unset BROWSER_FLAT
@@ -275,7 +288,7 @@ if [ "$FLATPAK" != flatpak ]; then
 		unset MAILC_FLAT
 	fi
 else
-	if [ $(echo "$BROWSER" | grep -o vivaldi-stable || true) ]; then
+	if [ "$CPUAAMD" = AuthenticAMD ]; then
 		if [ ! -f /etc/apt/sources.list.d/vivaldi.list ]; then 
 			aria2c -d /tmp https://repo.vivaldi.com/archive/linux_signing_key.pub
 			sudo apt-key add /tmp/linux_signing_key.pub
@@ -283,29 +296,63 @@ else
 			sudo apt update
 		fi
 		sudo apt install -y vivaldi-stable "$NIR"
+	elif [ "$CPUAAMD" = GenuineIntel ]; then
+		if [ ! -f /etc/apt/sources.list.d/vivaldi.list ]; then 
+			aria2c -d /tmp https://repo.vivaldi.com/archive/linux_signing_key.pub
+			sudo apt-key add /tmp/linux_signing_key.pub
+			echo "deb https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
+			sudo apt update
+		fi
+		sudo apt install -y vivaldi-stable "$NIR"
+	else
+		aria2c -d /tmp https://downloads.vivaldi.com/snapshot/install-vivaldi.sh
+		sudo sh /tmp/install-vivaldi --final
 	fi
 fi
 
 # If you choose to install virtualbox, this script will install the current (6.1) version of it.
 if [ "$VBOX" = virtualbox-6.1 ]; then
-	if [ $(dpkg-query -W -f='${Status}' virtualbox-6.1 2>/dev/null | grep -c "ok installed" || true) -eq 0 ]; then
-		sudo apt install -y apt-transport-https
-		if [ "$(uname -r | grep -o 5.18 || true )" != 5.18  ]; then
-			if [ ! -f /etc/apt/sources.list.d/virtualbox.list ]; then
-				aria2c -d /tmp https://www.virtualbox.org/download/oracle_vbox_2016.asc
-				aria2c -d /tmp https://download.virtualbox.org/virtualbox/6.1.34/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
-				sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg /tmp/oracle_vbox_2016.asc
-				echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list >/dev/null
-				sudo apt update
+	if [ "$CPUAAMD" = AuthenticAMD ]; then
+			if [ $(dpkg-query -W -f='${Status}' virtualbox-6.1 2>/dev/null | grep -c "ok installed" || true) -eq 0 ]; then
+			sudo apt install -y apt-transport-https
+			if [ "$(uname -r | grep -o 5.18 || true )" != 5.18  ]; then
+				if [ ! -f /etc/apt/sources.list.d/virtualbox.list ]; then
+					aria2c -d /tmp https://www.virtualbox.org/download/oracle_vbox_2016.asc
+					aria2c -d /tmp https://download.virtualbox.org/virtualbox/6.1.34/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
+					sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg /tmp/oracle_vbox_2016.asc
+					echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list >/dev/null
+					sudo apt update
+				fi
+				sudo apt install -y "$VBOX" "$NIR"
+				echo "y" | sudo VBoxManage extpack install /tmp/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
+				sudo usermod "$USER" -aG vboxusers
+				echo ""; echo 'You have been added to the "vboxusers" group.'
+				sleep 3
+			else
+				echo ""; echo "Virtualbox currently does not run with the Linux 5.18 kernel,"; echo "so it won't be installed."
+				sleep 3
 			fi
-			sudo apt install -y "$VBOX" "$NIR"
-			echo "y" | sudo VBoxManage extpack install /tmp/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
-			sudo usermod "$USER" -aG vboxusers
-			echo ""; echo 'You have been added to the "vboxusers" group.'
-			sleep 3
-		else
-			echo ""; echo "Virtualbox currently does not run with the Linux 5.18 kernel,"; echo "so it won't be installed."
-			sleep 3
+		fi
+	elif [ "$CPUAAMD" = GenuineIntel ]; then
+			if [ $(dpkg-query -W -f='${Status}' virtualbox-6.1 2>/dev/null | grep -c "ok installed" || true) -eq 0 ]; then
+			sudo apt install -y apt-transport-https
+			if [ "$(uname -r | grep -o 5.18 || true )" != 5.18  ]; then
+				if [ ! -f /etc/apt/sources.list.d/virtualbox.list ]; then
+					aria2c -d /tmp https://www.virtualbox.org/download/oracle_vbox_2016.asc
+					aria2c -d /tmp https://download.virtualbox.org/virtualbox/6.1.34/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
+					sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg /tmp/oracle_vbox_2016.asc
+					echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list >/dev/null
+					sudo apt update
+				fi
+				sudo apt install -y "$VBOX" "$NIR"
+				echo "y" | sudo VBoxManage extpack install /tmp/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack
+				sudo usermod "$USER" -aG vboxusers
+				echo ""; echo 'You have been added to the "vboxusers" group.'
+				sleep 3
+			else
+				echo ""; echo "Virtualbox currently does not run with the Linux 5.18 kernel,"; echo "so it won't be installed."
+				sleep 3
+			fi
 		fi
 	fi
 fi
